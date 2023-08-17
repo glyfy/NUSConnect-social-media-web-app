2cf10f77e60bf1acdd2fbf7dbda2e9c92b3394aa
GlenLim <glenlimyf@gmail.com>
2023-08-17 18:14:11 +0800

    Update config axios baseURL

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a new Axios instance with custom configuration
 export const axiosInstance = axios.create({
-  baseURL: 'https://nusconnect.onrender.com/',
+  baseURL: 'https://nusconnect.onrender.com/api',
   // You can add more configuration options here
 });
 
